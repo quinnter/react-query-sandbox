3d586bda8b6e68a1b054e2cb041a7dfd28100150
quinnter <lauraaquinn16@gmail.com>
2022-06-24 13:22:28 +0100

    Add initialDataUpdatedAt to instantly refresh initial data instead of waiting for it to go stale

--- src/queries.ts
+++ src/queries.ts
@@ -2,4 +2,12 @@ import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import * as api from './api'
 
 // Queries
-export const useCoronaVirusData = () => useQuery('coronaVirusData', api.getCoronaVirusData, { staleTime: 300000, initialData: { cache: { expiresTimestamp: 0}} })
+export const useCoronaVirusData = () => useQuery(
+    'coronaVirusData', 
+    api.getCoronaVirusData, 
+    { 
+        staleTime: 300000, 
+        initialData: { cache: { expiresTimestamp: 0}},
+        initialDataUpdatedAt: 1608412420052
+    }
+)

5e0416fed03a99592e320ed2bf0ea2505d9bd0fc
quinnter <lauraaquinn16@gmail.com>
2022-06-24 13:10:43 +0100

    Set up initial data to fix first component mount breaking

--- src/queries.ts
+++ src/queries.ts
@@ -2,4 +2,4 @@ import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import * as api from './api'
 
 // Queries
-export const useCoronaVirusData = () => useQuery('coronaVirusData', api.getCoronaVirusData, { staleTime: 300000 })
+export const useCoronaVirusData = () => useQuery('coronaVirusData', api.getCoronaVirusData, { staleTime: 300000, initialData: { cache: { expiresTimestamp: 0}} })
